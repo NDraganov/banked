227bad26ff51ba56604b2ecd410a88e9899aa32f
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2024-07-08 01:15:09 +0100

    Add sign up inputs to the auth form

--- components/auth/AuthForm.tsx
+++ components/auth/AuthForm.tsx
@@ -35,6 +35,54 @@ export default function AuthForm({ type }: { type: string }) {
       </header>
       <Form {...form}>
         <form>
+          {type === "sign-up" && (
+            <>
+              <div className="flex justify-between">
+                <FormInput
+                  control={form.control}
+                  name="firstName"
+                  label="First Name"
+                  placeholder="Enter your first name"
+                />
+                <FormInput
+                  control={form.control}
+                  name="lastName"
+                  label="Last Name"
+                  placeholder="Enter your first name"
+                />
+              </div>
+              <FormInput
+                control={form.control}
+                name="address"
+                label="Address"
+                placeholder="Enter your specific address"
+              />
+
+              <div className="flex gap-4 justify-between">
+                <FormInput
+                  control={form.control}
+                  name="city"
+                  label="City"
+                  placeholder="Enter your city"
+                />
+                <FormInput
+                  control={form.control}
+                  name="postCode"
+                  label="Post Code"
+                  placeholder="Example: 11101"
+                />
+              </div>
+              <div className="flex gap-4">
+                <FormInput
+                  control={form.control}
+                  name="dateOfBirth"
+                  label="Date of Birth"
+                  placeholder="YYYY-MM-DD"
+                />
+              </div>
+            </>
+          )}
+
           <FormInput
             control={form.control}
             name="email"
